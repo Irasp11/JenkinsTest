7fc4c112a81bc61213344fc2f8464184eda24b37
Irasp Andreasian <iandreasian@neoflex.ru>
2025-04-30 11:39:04 +0300

    second commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("io.qameta.allure") version "2.9.6"
+    id("io.freefair.lombok") version "8.2.0"
 }
 
 group = "cmc_neoflex.cloud"
@@ -10,8 +12,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.12.2")
+    testImplementation("io.rest-assured:rest-assured:5.5.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
+    implementation("io.qameta.allure:allure-rest-assured:2.29.1")
+    testImplementation("io.qameta.allure:allure-junit5:2.29.1")
+
 }
 
 tasks.test {
---
+++ src/main/java/MemberPojo.java
@@ -0,0 +1,14 @@
+import lombok.Data;
+
+import java.util.ArrayList;
+@Data
+public class MemberPojo {
+    private String name;
+    private String agency;
+    private String image;
+    private String wikipedia;
+    private ArrayList<String> launches;
+    private String status;
+    private String id;
+}
+
